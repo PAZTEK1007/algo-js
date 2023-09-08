165dd39c5b934f4d4645d5a6668440d78cc86efd
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-08 11:47:20 +0200

    First Exercices Push

---
+++ 1.2.js
@@ -0,0 +1,4 @@
+let name = "Barbier";
+let firstname = "Mathias";
+let city = "Ecaussinnes";
+console.log("My firstname is " + firstname + " and my last name is " + name + " and i live in " + city);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# algo-js
-Algo Javascript
+# ALGO Javascript Exercices
+## Exercices 1 :
+Files : - 1.0.js
+        + 1.1.js
+        - 1.2.js
