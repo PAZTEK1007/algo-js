2c23344cb11e0ce0ba3b400d309841b5e33ea046
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-11 16:03:28 +0200

    del exercices2 Files

--- 2.1.js
+++
@@ -1,9 +0,0 @@
-const log = Number(window.prompt("Whats your age ?"));
-let age1 = 18;
-
-if (log >= age1) {
-    console.log("You are an adult");
-
-} else {
-    console.log("You are not yet an adult");
-} 
