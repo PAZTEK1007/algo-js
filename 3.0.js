278749e43d63c27407ee16b306aeac958e53d7e9
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-11 17:40:42 +0200

    Del exercices3 files and push 4.0.js files and update html

--- 3.0.js
+++
@@ -1,5 +0,0 @@
-let stringsArray = ["Mathias", "Antoine", "Pierre", "Thomas", "Alexande.V", "Alexande", "Bastien", "Carole" , "Dorian", "Elisabeth" , "Elodie", "Justin", "Justine L" , "Justine" , "Kimi", "Layla", "Lidwine", "Lucas" , "Marie", "Okly", "Robin", "Rosalie", "Stephane", "Tim", "Valentin" ,"Virginie"];
-
-for (let elem of stringsArray){
-    console.log(elem);
-}
