1400f24692b23f053327e59ceb905297b371c204
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-13 16:45:19 +0200

    Del files

--- ex1/1.2.js
+++
@@ -1,4 +0,0 @@
-let name = "Barbier";
-let firstname = "Mathias";
-let city = "Ecaussinnes";
-console.log("My firstname is " + firstname + " and my last name is " + name + " and i live in " + city);
