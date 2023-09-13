68271296bf55dcedde2213ed4041928f73ad3c58
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-13 10:41:31 +0200

    Del 4.1.js

--- 4.1.js
+++
@@ -1,9 +0,0 @@
-function calcSurface (a, b){
-
-    return a * b;
-}
-
-let long = +window.prompt("Ecris une longueur !");
-let large = +window.prompt("Ecris une largeur !");
-let result = calcSurface(long, large);
-alert("Le rectancgle à une surface de " + result);
